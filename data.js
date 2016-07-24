98c98923efc74c11c632e3efb84e58fd9305cd30
cuixiao <cuixiao1008@126.com>
2016-07-24 22:29:14 +0800

    finish project

---
+++ data.js
@@ -0,0 +1,111 @@
+ var data = [
+
+     {
+      "img" :"1.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+     {
+      "img" :"2.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"3.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"4.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"5.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+
+     
+     {
+      "img" :"6.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"7.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"8.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"9.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+
+     
+     {
+      "img" :"10.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"11.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"12.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"13.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+
+     
+     {
+      "img" :"14.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"15.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"16.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"17.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+
+     
+     {
+      "img" :"18.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"19.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      },
+       {
+      "img" :"20.jpg" ,
+      "caption" :"我和我妈妈 ",
+      "desc" :"lorem lorem lorem"
+      }
+ ];
